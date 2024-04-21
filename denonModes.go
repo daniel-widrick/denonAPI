bfd7b1d0133991de0b6687717be58d567d491ef4
lvlint67 <daniel@widrick.net>
2024-04-21 01:44:40 -0400

    add stereo and direct options

--- denonModes.go
+++ denonModes.go
@@ -8,29 +8,66 @@ import (
 	"time"
 )
 
-var con net.Conn
-
 // https://github.com/bencouture/denon-rest-api/blob/master/protocol.pdf
 func main(){
 	mux := http.NewServeMux()
 	mux.Handle("/dolbyMovie",&DolbyHandler{})
+	mux.Handle("/stereo",&StereoHandler{})
+	mux.Handle("/direct",&DirectHandler{})
 	http.ListenAndServe(":8080",mux)
 
 }
 
 type DolbyHandler struct{}
 func(d *DolbyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	dolbyMovie()
+}
+
+type StereoHandler struct{}
+func(d *StereoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	stereo()
+}
+
+type DirectHandler struct{}
+func(d *DirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	direct()
+}
+
+func avrConnect() net.Conn {
 	const AVR_ADDR = "192.168.67.246:23"
 	con, err := net.Dial("tcp",AVR_ADDR)
 	if err != nil {
 		log.Fatalf("Cannot connect to %s : %s\n",AVR_ADDR,err)
 	}
-	defer con.Close()
 	fmt.Println("Connected to AVR")
-	dolbyMovie(con)
+	return con
 }
 
-func dolbyMovie(con net.Conn){
+func direct() {
+	con := avrConnect()
+	defer con.Close()
+
+	fmt.Println("Switching to Direct")
+	data := []byte("MSDIRECT\r")
+	sendCommand(data,con)
+}
+
+func stereo(){
+	con := avrConnect()
+	defer con.Close()
+
+	fmt.Println("Switching to Stero")
+	data := []byte("MSSTEREO\r")
+	sendCommand(data,con)
+
+	fmt.Println("Turning off Cinema EQ")
+	data = []byte("PSCINEMA EQ.OFF\r")
+	sendCommand(data,con)
+}
+
+func dolbyMovie(){
+	con := avrConnect()
+	defer con.Close()
 	fmt.Println("Switching to TV Input")
 	data := []byte("SITV\r")
 	sendCommand(data,con)
